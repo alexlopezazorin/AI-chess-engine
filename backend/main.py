3dd11f4881dae4a27fd9fa1240ea72e79f37b029
alexlopezazorin <alexlopezazorin@gmail.com>
2026-06-24 16:04:48 +0200

    bug fixed

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 import uvicorn
+import os
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.api.api:app", host="127.0.0.1", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("app.api.api:app", host="0.0.0.0", port=port)
