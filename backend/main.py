ac410a8783b6700adc6c081e089da5f113f4ad5e
alexlopezazorin <alexlopezazorin@gmail.com>
2026-06-24 16:12:27 +0200

    debugging

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 import os
 
-
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("app.api.api:app", host="0.0.0.0", port=port)
+    print(f"Starting server on 0.0.0.0:{port}")
+    uvicorn.run("app.api.api:app", host="0.0.0.0", port=port, log_level="info")
